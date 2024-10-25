890b35ef8723a108ad5108410f00c438ec351121
Spaseyhin <evgen.kalin99@gmail.com>
2024-10-25 19:00:19 +0400

    Add RuboCop and RSpec setup, fix style issues across the project

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
+# The ApplicationHelper module provides utility methods
+# that can be used across views in the application.
 module ApplicationHelper
+  # Methods will be added here
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
+# ApplicationMailer serves as the base class for all mailers in the application.
+# It defines common settings and behavior for outgoing emails.
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
-  layout "mailer"
+  default from: 'from@example.com'
+  layout 'mailer'
 end
