ca47baa710e0253c144a9dbf0c50e7846abe1bf6
Spaseyhin <evgen.kalin99@gmail.com>
2025-02-18 18:56:31 +0500

    chore: update controllers index

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,3 +2,5 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
+import FormValidationController from "./form_validation_controller"
+application.register("form-validation", FormValidationController)
