da34d1afcd72e8c2c7ce556e185503cb7474b65e
Spaseyhin <evgen.kalin99@gmail.com>
2025-03-09 17:03:36 +0500

    Add models and migrations for cart system

---
+++ db/migrate/20250309111149_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.decimal :price
+      t.text :description
+      t.string :image_url
+      t.integer :weight
+
+      t.timestamps
+    end
+  end
+end
