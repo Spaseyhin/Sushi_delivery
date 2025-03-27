c06798a808b26fb34d7e532419c98fe9dfe28139
Spaseyhin <evgen.kalin99@gmail.com>
2025-03-27 12:02:48 +0500

    Added model for order and product relation

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,11 @@
 class Order < ApplicationRecord
   belongs_to :user
+  has_many :order_items, dependent: :destroy
+
+  after_create_commit lambda {
+    broadcast_prepend_later_to 'orders',
+                               target: 'orders',
+                               partial: 'admin/orders/order',
+                               locals: { order: self }
+  }
 end
