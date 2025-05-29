a68d157abd8431f3649969c5785820809561802f
Spaseyhin <evgen.kalin99@gmail.com>
2025-05-29 19:53:24 +0500

    chore: remove explicit pin for form_validation_controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,5 +2,3 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
-import FormValidationController from "./form_validation_controller"
-application.register("form-validation", FormValidationController)
